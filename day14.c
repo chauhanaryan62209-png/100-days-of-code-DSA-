7c154149018c714d1fdcfd5808f09d67fb44187d
chauhanaryan62209-png <chauhanaryan62209@gmail.com>
2026-05-01 20:47:45 +0530

    Create day14.c

---
+++ day14.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, isIdentity = 1;
+
+    scanf("%d", &n);
+
+    int matrix[n][n];
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    for(i = 0; i < n; i++) {
+        for(j = 0; j < n; j++) {
+            if(i == j && matrix[i][j] != 1)
+                isIdentity = 0;
+            if(i != j && matrix[i][j] != 0)
+                isIdentity = 0;
+        }
+    }
+
+    if(isIdentity)
+        printf("Identity Matrix");
+    else
+        printf("Not an Identity Matrix");
+
+    return 0;
+}
